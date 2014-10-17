76383850a4f142ec32874cc3646078b797d3242b
Michael Ochs <michael.ochs@hrs.de>
2014-10-17 15:25:21 +0200

    docs(section): add missing documentation to the section controller module
    close #4

--- Pod/Classes/HRSSectionController/HRSTableViewSectionCoordinator+IndexPathMapping.h
+++ Pod/Classes/HRSSectionController/HRSTableViewSectionCoordinator+IndexPathMapping.h
@@ -31,10 +31,26 @@
  @return the index path in the space of the passed in controller
  */
 - (NSIndexPath *)controllerIndexPathForTableViewIndexPath:(NSIndexPath *)tableViewIndexPath withController:(id<HRSTableViewSectionController>)controller;
+
+/**
+ Maps a section from the table view space to the given controller's section.
+ 
+ @note When calling this with the a controller and the section it is controlling
+       it will always return 0, however this is in preparation of the fact that
+       one day a section controller should be able to handle more than one
+       section. You can also use this method to calculate offsets between two
+       different section controllers.
+ 
+ @param tableViewSection The section you want to map in the table view's space.
+ @param controller       The controller the returned section should be in
+                         relation to.
+ 
+ @return The mapped section in the space of the passed-in section controller.
+ */
 - (NSInteger)controllerSectionForTableViewSection:(NSInteger)tableViewSection withController:(id<HRSTableViewSectionController>)controller;
 
 /**
- Maps an index path from the passed in controller's space to the table view
+ Maps an index path from the passed in controller's space to the table view.
  
  @param controllerIndexPath the index path used inside a section controller
  @param controller          the controller that the `controllerIndexPath`
@@ -43,6 +59,15 @@
  @return the index path in the space of the table view
  */
 - (NSIndexPath *)tableViewIndexPathForControllerIndexPath:(NSIndexPath *)controllerIndexPath withController:(id<HRSTableViewSectionController>)controller;
+
+/**
+ Maps a section from the passed in controller's space to the table view.
+ 
+ @param controllerSection the section used inside a section controller
+ @param controller        the controller that the `controllerSection` relates to
+ 
+ @return the section in the space of the table view
+ */
 - (NSInteger)tableViewSectionForControllerSection:(NSInteger)controllerSection withController:(id<HRSTableViewSectionController>)controller;
 
 @end
--- Pod/Classes/HRSSectionController/Private/_HRSTableViewSectionCoordinatorProxy.h
+++ Pod/Classes/HRSSectionController/Private/_HRSTableViewSectionCoordinatorProxy.h
@@ -20,20 +20,84 @@
 @protocol HRSTableViewSectionController;
 
 
+/**
+ A table view section coordinator proxy is responsible for mapping values
+ between a section controller and the corresponding table view based on the
+ mapping information from the controller's section coordinator.
+ 
+ A proxy has a defined direction. It either maps from a table view to a section
+ controller or in the other direction, but not both. You can easily create a
+ reverse proxy by calling `reverseProxy`.
+ 
+ Mapping is done based on the parameter and the data type this parameter has.
+ When mapping index paths, integers (sections) or index sets they are mapped
+ based on the information of the section controller's coordinator. When mapping
+ data types like a table view, they are mapped by creating a proxy for the
+ parameter that has the reverse order than the current proxy.
+ 
+ A proxy automatically has a reverse behaviour when mapping a return value. E.g.
+ when you are mapping the table view delegate method
+ `tableView:willSelectRowAtIndexPath:` you need a proxy that maps from the table
+ view to the section controller. This means the first and second argument are
+ mapped to the section controller's space. When you then return an index path
+ inside this method, the return value is automatically mapped back to the table
+ view from the section controller.
+ */
 @interface _HRSTableViewSectionCoordinatorProxy : NSProxy
 
+/**
+ Register a new selector that participates in object mapping.
+ 
+ The newly registered selector will immediately be taken into account by all
+ existing and new proxies.
+ 
+ You pass in an index set with the information about which parameters should be
+ mapped. Index path mapping starts with 0 as the selector and continues with 1
+ for the first argument. Please refer to
+ `+[HRSTableViewSectionCoordinator registerTransformer:arguments:]` for more
+ information about how mapping is configured.
+ 
+ @see +[HRSTableViewSectionCoordinator registerTransformer:arguments:]
+ 
+ @param selector The selector you want to register.
+ @param indexSet The index set with the mapping information.
+ */
 + (void)registerSelector:(SEL)selector arguments:(NSIndexSet *)indexSet;
 
 /**
  Creates a new proxy that mediates between the controller and the table view.
  
- @param controller the controller that should be used to map the index pathes
+ @param controller the controller that should be used to map the index paths
+ @param tableView  the table view that should be used to map the index paths
  
  @return a newly created instance of a table view proxy.
  */
 + (instancetype)proxyWithController:(id<HRSTableViewSectionController>)controller tableView:(UITableView *)tableView;
+
+/**
+ Creates a new proxy that mediates between the table view and the controller.
+ 
+ @note This is the reverse mapping of the method `proxyWithController:tableView:`.
+ 
+ @param controller the controller that should be used to map the index paths
+ @param tableView  the table view that should be used to map the index paths
+ 
+ @return a newly created instance of a section controller proxy.
+ */
 + (instancetype)reverseProxyWithController:(id<HRSTableViewSectionController>)controller tableView:(UITableView *)tableView;
 
+/**
+ Returns the reverse proxy of the current one.
+ 
+ This method always reverses the proxy behaviour. This means that you will get
+ the original behaviour when you call this method on a reversed proxy. It simply
+ toggles between the two directions.
+ 
+ @note currently this method creates a new instance of the proxy. Do not count
+	   on this behaviour as this is subject to change due to optimisations!
+ 
+ @return a proxy that has the reverse mapping direction than the receiver.
+ */
 - (instancetype)reverseProxy;
 
 @end
